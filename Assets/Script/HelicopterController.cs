2be7f0c9aa78930c9d7ddbec09af45f10dcd7591
thnphu <thnphu1@gmail.com>
2025-02-22 21:49:09 +1100

    add GameManager.cs

--- Assets/Script/HelicopterController.cs
+++ Assets/Script/HelicopterController.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HelicopterController : MonoBehaviour
 {
+    public float speed = 5f;
+
+    public int maxCapacity = 3;
+
+    private int currentSoldiers = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -14,6 +21,26 @@ public class HelicopterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(new Vector3(moveX, moveY, 0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Soldier") && currentSoldiers < maxCapacity)
+        {
+            Destroy(other.gameObject);
+            currentSoldiers++;
+        }
+        else if (other.CompareTag("Hospital"))
+        {
+            currentSoldiers = 0;
+        }
+        else if (other.CompareTag("Tree"))
+        {
+            Debug.Log("Game Over!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
     }
 }
